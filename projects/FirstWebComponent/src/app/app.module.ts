e84dc93167eb7104baed96cab89fec91003ab890
Armen Vardanyan <armenvardanyan95@gmail.com>
2020-05-29 18:25:32 +0400

    Initial commit

---
+++ projects/FirstWebComponent/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, DoBootstrap, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { UIButtonComponent } from './uibutton/uibutton.component';
+
+@NgModule({
+  declarations: [
+    UIButtonComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  entryComponents: [UIButtonComponent],
+})
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector) {
+    const webComponent = createCustomElement(UIButtonComponent, {injector});
+    customElements.define('ui-button', webComponent);
+  }
+
+  ngDoBootstrap() {}
+ }
